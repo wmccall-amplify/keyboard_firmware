6a7fb914dd37260ada79b70b3f9929e02c43eb8b
wmccall-amplify <wmccall@amplify.com>
2019-05-31 15:25:03 -0400

    Fix styling

--- keyboards/sentraq/s65_x/keymaps/wmccall/keymap.c
+++ keyboards/sentraq/s65_x/keymaps/wmccall/keymap.c
@@ -98,10 +98,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [_MFL] = LAYOUT_65_ansi(
-    KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  G(KC_BSPC), KC_PWR , \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAD, RGB_VAI, RGB_TOG,    BL_TOGG, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_HUD, RGB_HUI,          RGB_MOD,    BL_INC , \
-    _______,          _______, _______, _______, _______, _______, _______, _______, _______, RGB_SAD, RGB_SAI, KC_MPLY, KC_VOLU,    BL_DEC , \
+    KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  G(KC_BSPC), KC_PWR , \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, RGB_VAD, RGB_VAI, RGB_TOG,    BL_TOGG, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  RGB_HUD, RGB_HUI,          RGB_MOD,    BL_INC , \
+    _______,          _______, _______, _______, _______, _______, _______, _______, _______,  RGB_SAD, RGB_SAI, KC_MPLY, KC_VOLU,    BL_DEC , \
     RESET  , _______, _______,                   _______,                            TO(_MLD), _______, TO(_BL), KC_MPRV, KC_VOLD,    KC_MNXT  \
   ),
 
@@ -122,10 +122,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* 4: ANSI DVORAK */
   [_BLD] = LAYOUT_65_ansi(
-    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,  \
-    KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,     KC_SLSH, KC_EQL,  KC_BSLS, KC_INS , \
-    KC_CAPS, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,     KC_MINS,          KC_ENT,  KC_PGUP, \
-    KC_LSFT,          KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,     KC_Z,    KC_RSFT, KC_UP,   KC_PGDN, \
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,      KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,  \
+    KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,      KC_SLSH, KC_EQL,  KC_BSLS, KC_INS,  \
+    KC_CAPS, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,      KC_MINS,          KC_ENT,  KC_PGUP, \
+    KC_LSFT,          KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,      KC_Z,    KC_RSFT, KC_UP,   KC_PGDN, \
     KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(_BFLD), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
@@ -147,8 +147,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BFLD] = LAYOUT_65_ansi(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_SLEP, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_VAD, RGB_VAI, RGB_TOG, BL_TOGG, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_HUD, RGB_HUI,          RGB_MOD, BL_INC , \
-    _______,          _______, _______, _______, _______, _______, _______, _______, _______, RGB_SAD, RGB_SAI, KC_MPLY, KC_VOLU, BL_DEC , \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_HUD, RGB_HUI,          RGB_MOD, BL_INC,  \
+    _______,          _______, _______, _______, _______, _______, _______, _______, _______, RGB_SAD, RGB_SAI, KC_MPLY, KC_VOLU, BL_DEC,  \
     RESET  , _______, _______,                   _______,                            TO(_ML), _______, TO(_BL), KC_MPRV, KC_VOLD, KC_MNXT  \
   ),
 
@@ -168,10 +168,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* 6: ANSI DVORAK */
   [_MLD] = LAYOUT_65_ansi(
-    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,  \
-    KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,     KC_SLSH, KC_EQL,  KC_BSLS, KC_INS , \
-    KC_CAPS, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,     KC_MINS,          KC_ENT,  KC_PGUP, \
-    KC_LSFT,          KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,     KC_Z,    KC_RSFT, KC_UP,   KC_PGDN, \
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,      KC_LBRC, KC_RBRC, KC_BSPC, KC_DEL,  \
+    KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,      KC_SLSH, KC_EQL,  KC_BSLS, KC_INS , \
+    KC_CAPS, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,      KC_MINS,          KC_ENT,  KC_PGUP, \
+    KC_LSFT,          KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,      KC_Z,    KC_RSFT, KC_UP,   KC_PGDN, \
     KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RALT, MO(_MFLD), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
