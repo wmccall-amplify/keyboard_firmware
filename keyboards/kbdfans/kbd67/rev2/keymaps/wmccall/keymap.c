579868518d84eb0176807a631708033c1c5d2b36
wmccall-amplify <wmccall@amplify.com>
2020-08-01 17:51:13 -0400

    Add new layout for kbd67

--- keyboards/kbdfans/kbd67/rev2/keymaps/wmccall/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/wmccall/keymap.c
@@ -1,9 +1,20 @@
+/* Copyright 2018 'mechmerlin'
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include QMK_KEYBOARD_H
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 #define _BL 0    // Base Layer
 #define _BFL 1   // Base Fn Layer
 #define _ML 2    // Mac Layer
@@ -14,6 +25,15 @@
 
 // Macro to perform ctrl + shift + esc
 #define KC_CSE LSFT(LCTL(KC_ESC))
+
+// Macro to sleep mac (ctrl + shift + power)
+#define KC_MAC_SLEEP LSFT(LCTL(KC_PWR))
+
+// Defines the keycodes used by our macros in process_record_user
+enum custom_keycodes {
+  QMKBEST = SAFE_RANGE,
+  QMKURL
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: Base Layer (Default Layer)
@@ -29,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Win |Alt |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
-[_BL] = LAYOUT_ansi(
+[_BL] = LAYOUT_65_ansi(
   KC_GESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_DEL, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_PGUP, \
   KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,KC_PGDN,  \
@@ -40,23 +60,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------.
    * | ~ | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|       |slp |
    * |----------------------------------------------------------------|
-   * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |    |
+   * |     |   |   |   |   |   |   |   |   |   |   |rv-|rv+|RGBtg|    |
    * |----------------------------------------------------------------|
-   * |      |   |   |   |   |   |   |   |   |   |   |   |        |    |
+   * |      |   |   |   |   |   |   |   |   |   |rh-|rh+| RGBmod |    |
    * |----------------------------------------------------------------|
-   * |        |   |   |   |   |   |   |   |   |   |   |pl/pse|vup|mute|
+   * |        |   |   |   |   |   |   |   |   │rs-│rs+│pl/pse|vup|mute|
    * |----------------------------------------------------------------|
    * |TMgr|    | CAD|                       |_ML|   |_BL |prv|vdn| nxt|
    * `----------------------------------------------------------------'
    */
-[_BFL] = LAYOUT_ansi(
+[_BFL] = LAYOUT_65_ansi(
    KC_GRV,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,_______,KC_SLEP, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______,_______, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_MPLY, KC_VOLU,KC_MUTE, \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RGB_VAD, RGB_VAI, RGB_TOG,_______, \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RGB_HUD,RGB_HUI,          RGB_MOD,_______, \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,RGB_SAD,RGB_SAI,KC_MPLY, KC_VOLU,KC_MUTE, \
    KC_CSE,_______, KC_CAD,                 _______,               TO(_ML),_______,TO(_BL),KC_MPRV, KC_VOLD, KC_MNXT),
 
-  /* Keymap _ML: Mac Layer
+/* Keymap _ML: Mac Layer
    * ,----------------------------------------------------------------.
    * |Esc | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Del |
    * |----------------------------------------------------------------|
@@ -69,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl|Alt |Alt |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
    * `----------------------------------------------------------------'
    */
-[_ML] = LAYOUT_ansi(
+[_ML] = LAYOUT_65_ansi(
   KC_GESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC,KC_DEL, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS,KC_PGUP, \
   KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,KC_PGDN,  \
@@ -80,19 +100,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------.
    * | ~ | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Cmd+Del|PWR |
    * |----------------------------------------------------------------|
-   * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |    |
+   * |     |   |   |   |   |   |   |   |   |   |   |rv-|rv+|RGBtg|    |
    * |----------------------------------------------------------------|
-   * |      |   |   |   |   |   |   |   |   |   |   |   |        |    |
+   * |      |   |   |   |   |   |   |   |   |   |rh-|rh+| RGBmod |    |
    * |----------------------------------------------------------------|
-   * |        |   |   |   |   |   |   |   |   |   |   |pl/pse|vup|mute|
+   * |        |   |   |   |   |   |   |   |   │rs-│rs+│pl/pse|vup|mute|
    * |----------------------------------------------------------------|
    * |    |    |    |                       |_ML|   |_BL |prv|vdn| nxt|
    * `----------------------------------------------------------------'
    */
-[_MFL] = LAYOUT_ansi(
+[_MFL] = LAYOUT_65_ansi(
    KC_GRV,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,G(KC_BSPC),KC_PWR , \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______,_______, \
-  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_MPLY, KC_VOLU,KC_MUTE, \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RGB_VAD, RGB_VAI, RGB_TOG,_______, \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,RGB_HUD,RGB_HUI,          RGB_MOD,_______, \
+  _______,_______,_______,_______,_______,_______,_______,_______,_______,RGB_SAD,RGB_SAI,KC_MPLY, KC_VOLU,KC_MUTE, \
   _______,_______,_______,                 _______,               TO(_ML),_______,TO(_BL),KC_MPRV, KC_VOLD, KC_MNXT),
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QMKBEST:
+      if (record->event.pressed) {
+        // when keycode QMKBEST is pressed
+        SEND_STRING("QMK is the best thing ever!");
+      } else {
+        // when keycode QMKBEST is released
+      }
+      break;
+    case QMKURL:
+      if (record->event.pressed) {
+        // when keycode QMKURL is pressed
+        SEND_STRING("https://qmk.fm/" SS_TAP(X_ENTER));
+      } else {
+        // when keycode QMKURL is released
+      }
+      break;
+  }
+  return true;
+}
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+
+}
+
+void led_set_user(uint8_t usb_led) {
+
+}
